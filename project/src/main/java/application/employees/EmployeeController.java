ada7e12a5c4a7ee3a79e47e6ad417c3dfae5783e
deChaplin <deChaplin@duck.com>
2023-12-24 21:51:22 +0000

    Added fxml files and java files for each page

---
+++ project/src/main/java/application/employees/EmployeeController.java
@@ -0,0 +1,43 @@
+package application.employees;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class EmployeeController {
+
+    private Stage stage;
+    private Scene scene;
+
+    public void CloseApplication(ActionEvent event) throws IOException {
+        // Close the application
+        System.exit(0);
+    }
+
+    public void openDashboard(ActionEvent event) throws IOException {
+
+    }
+
+    public void openSchedule(ActionEvent event) throws IOException {
+
+    }
+
+    public void openPayroll(ActionEvent event) throws IOException {
+
+    }
+
+    public void openPeople(ActionEvent event) throws IOException {
+
+    }
+
+    public void openTimeoff(ActionEvent event) throws IOException {
+
+    }
+
+    public void openHelp(ActionEvent event) throws IOException {
+
+    }
+
+}
